6c41f5160b21beeb6689851ca5c6c1d8cce62051
KubaMariaMazurkiewicz <kuba.mazurkiewicz@gmail.com>
2026-01-19 18:25:44 +0100

    125

--- install-hugo.sh
+++ install-hugo.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
 
-HUGO_VERSION=0.114.3 # replace with your Hugo version
+HUGO_VERSION=0.114.3  # Change to your Hugo version
 
-# Download Hugo Linux 64-bit extended binary
-curl -sL -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+# Download Hugo extended Linux 64-bit tar.gz
+curl -sL -o hugo.tar.gz "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz"
 
-# Extract binary
+# Extract the binary
 tar -xzf hugo.tar.gz hugo
 
 # Make it executable
 chmod +x hugo
 
-# Move to ./node_modules/.bin so npm scripts can see it
+# Put it in node_modules/.bin so npm scripts can see it
 mkdir -p node_modules/.bin
 mv hugo node_modules/.bin/
+
+# Clean up
+rm hugo.tar.gz
